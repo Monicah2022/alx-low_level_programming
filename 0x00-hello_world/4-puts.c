d40b14f0b19006ed83dbebd505183b5e9d4ef722
Monicah Wangari <monicahw27@gmail.com>
2022-09-08 15:33:07 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
+#!include <stdio.h>
 
 /**
- * main - Printing
- *
- * Return: Always 0 (Successful)
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
  */
 
 int main(void)
 {
-     puts("\"Programming is like building a mulilingual puzzle");
-     
-     return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
