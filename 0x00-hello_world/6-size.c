53e5a83989b33e1ba1eac68cb8bb4d2018eb083d
Monicah Wangari <monicahw27@gmail.com>
2022-09-08 16:48:57 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,8 @@
  *
  * Return: Always 0 (success)
  */
-int main (void)
+int main(void)
+
 {
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of an int: %zu byte(s)\n", sizeof(int));
