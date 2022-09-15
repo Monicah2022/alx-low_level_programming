0db8b71e947bce09769dde2847ed23f940b10ac0
Monicah Wangari <monicahw27@gmail.com>
2022-09-15 15:09:01 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,12 +11,13 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
+
 	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
 	else if (b >= a && b >= c)
-	{
+	{i
 		largest = b;
 	}
 	else
--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,1 +1,3 @@
 Debugging tasks
+This program prints the largest of three integers
+This program converts a date to the day of year
